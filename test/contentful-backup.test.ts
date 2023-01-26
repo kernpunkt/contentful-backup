e49bdbad0c97c548b03f330e357a19093126d83d
Joern Meyer <joern.meyer@kernpunkt.de>
2023-01-26 13:57:59 +0100

    Initial commit

---
+++ test/contentful-backup.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ContentfulBackup from '../lib/contentful-backup-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/contentful-backup-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ContentfulBackup.ContentfulBackupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
